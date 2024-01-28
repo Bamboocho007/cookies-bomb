32cd2c4e632493ac5c56d03acba640bc132fbeda
Avisalon <bamboocho007@gmail.com>
2024-01-28 16:04:40 +0200

    fix migration

--- migrations/000003_table-restrictions.up.sql
+++ migrations/000003_table-restrictions.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE users
-ADD CONSTRAINT UniqueEmail UNIQUE (id, email);
+ADD CONSTRAINT UniqueEmail UNIQUE (email);
 
 ALTER TABLE user_securities
 ADD CONSTRAINT FK_PersonSequrities FOREIGN KEY (user_id) REFERENCES users(id);
